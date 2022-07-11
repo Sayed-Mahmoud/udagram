d452f11dada92a1d750185fe7db7156a394d498c
Sayed Mahmoud <sayedmahmoudgomaa1@gmail.com>
2022-07-11 07:17:27 +0200

    elasticbeanstalk-us-east-1-004493430015

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://sayedbucket2022/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://sayedbucket2022/
+aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-004493430015/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-004493430015/
