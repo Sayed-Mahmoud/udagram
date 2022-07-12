5a2e2751868c861e16a88727fc2f5ff7d6fa0c73
Sayed Mahmoud <sayedmahmoudgomaa1@gmail.com>
2022-07-13 01:41:14 +0200

    be

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./www/Archive.zip s3://elasticbeanstalk-us-east-1-004493430015/
+aws s3 cp --recursive --acl public-read ./www/ s3://elasticbeanstalk-us-east-1-004493430015/
