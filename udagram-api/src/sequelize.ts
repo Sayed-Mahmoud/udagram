92c2aa43ba4234b4e553395aef4a25971a3f46b8
Sayed Mahmoud <sayedmahmoudgomaa1@gmail.com>
2022-07-12 17:55:08 +0200

    postgres

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize(
-  "postgres://postgres:321456Sa@database-1.cbmigqbnjgzj.us-east-1.rds.amazonaws.com:5432/database-1"
+  "postgres://postgres:321456Sa@database-1.cbmigqbnjgzj.us-east-1.rds.amazonaws.com:5432/postgres"
 );
 /*
 export const sequelize = new Sequelize({
