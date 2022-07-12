ad78ff64f24e97106a4d7e35c1780036cf82c152
Sayed Mahmoud <sayedmahmoudgomaa1@gmail.com>
2022-07-12 04:14:53 +0200

    del .ebextensions

--- udagram-frontend/e2e/protractor.conf.js
+++ udagram-frontend/e2e/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://tests.us-east-1.elasticbeanstalk.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
