cfd13ce6ce17c24b57dc776416dac4356f92be2b
Sayed Mahmoud <sayedmahmoudgomaa1@gmail.com>
2022-07-12 17:48:47 +0200

    database-1

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,12 +1,17 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+export const sequelize = new Sequelize(
+  "postgres://postgres:321456Sa@database-1.cbmigqbnjgzj.us-east-1.rds.amazonaws.com:5432/database-1"
+);
+/*
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
-  host: config.host,
+  host: config.host + '',
   port: 5432,
   dialect: "postgres",
   storage: ":memory:",
 });
+*/
